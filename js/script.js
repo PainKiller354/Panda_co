d2b52cd82c5ad70e58b3becac96e4ce5522af4a7
PainKiller354 <vov.shabatura@gmail.com>
2022-08-07 18:19:20 +0300

    Added fonts.

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@ testWebP(function (support) {
     if (support == true) {
         document.querySelector('body').classList.add('webp');
     }
-})
+});
+
+function copytext(el) {
+    var $tmp = $("<textarea>");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+}
