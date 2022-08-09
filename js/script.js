c3bf835f26d681450bb66b4e0503febc4aaba941
PainKiller354 <vov.shabatura@gmail.com>
2022-08-09 20:21:17 +0300

    Added Back to top button. Added requisites.html and credentials section.

--- js/script.js
+++ js/script.js
@@ -10,4 +10,49 @@ testWebP(function (support) {
     if (support == true) {
         document.querySelector('body').classList.add('webp');
     }
-})
+});
+
+/*--------click_for_copy---------*/
+function copytext(el) {
+    var $tmp = $("<textarea>");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+}
+
+/*--------up_button---------*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
+
+/*-------up_button_loading_screen------*/
+jQuery(function($){
+    $(document).ajaxSend(function() {
+        $("#overlay").fadeIn(300);
+    });
+
+    $('#button').click(function(){
+        $.ajax({
+            type: 'GET',
+            success: function(data){
+                console.log(data);
+            }
+        }).done(function() {
+            setTimeout(function(){
+                $("#overlay").fadeOut(300);
+            },500);
+        });
+    });
+});
